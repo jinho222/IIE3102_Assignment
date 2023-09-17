24e327dc277cef116a368a766a80e78fcf236c96
jinho222 <jinho020@yonsei.ac.kr>
2023-09-17 22:16:34 +0900

    precit 2023-09-27 using statsmodels

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,111 +1,103 @@
-"""
-
-data source:
- - [기상청 기상자료개방포털](https://data.kma.go.kr/data/grnd/selectAsosRltmList.do?pgmNo=36&tabNo=1)
-
-"""
-
+import numpy as np
 import pandas as pd
+
 from matplotlib import pyplot as plt
 
-DAY = {
-    'ko': '일시',
-    'en': 'day'
-}
+from statsmodels.tsa.ar_model import AutoReg 
 
-AVERAGE_TEMPERATURE = {
-    'ko': '평균기온(°C)',
-    'en': 'average_temperature'
-}
+DAY = '일시'
+AVERAGE_TEMPERATURE = '평균기온(°C)'
+LOWEST_TEMPERATURE = '최저기온(°C)'
+HIGHEST_TEMPERATURE = '최고기온(°C)'
+# DAILY_PERCIPITATION = '일강수량(mm)' # 일강수량은 결측치가 많아서 사용하지 않는다. 
+HIGHEST_WIND_SPEED = '최대 풍속(m/s)'
+AVERAGE_WIND_SPEED = '평균 풍속(m/s)'
+AVERAGE_RELATIVE_HUMIDITY = '평균 상대습도(%)'
 
-LOWEST_TEMPERATURE = {
-    'ko': '최저기온(°C)',
-    'en': 'lowest_temperature'
-}
+def get_dataframe():
+    """
+    data source:
+    - [기상청 기상자료개방포털](https://data.kma.go.kr/data/grnd/selectAsosRltmList.do?pgmNo=36&tabNo=1)
+    """
+    ENCODING = 'cp949' # utf8 makes encoding issue. @see https://zephyrus1111.tistory.com/39
+    df_2013 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2013_2013_2015.csv', encoding=ENCODING)
+    df_2014 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2014_2014_2015.csv', encoding=ENCODING)
+    df_2015 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2015_2015_2016.csv', encoding=ENCODING)
+    df_2016 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2016_2016_2017.csv', encoding=ENCODING)
+    df_2017 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2017_2017_2018.csv', encoding=ENCODING)
+    df_2018 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2018_2018_2019.csv', encoding=ENCODING)
+    df_2019 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2019_2019_2020.csv', encoding=ENCODING)
+    df_2020 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2020_2020_2021.csv', encoding=ENCODING)
+    df_2021 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2021_2021_2022.csv', encoding=ENCODING)
+    df_2022 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2022_2022_2023.csv', encoding=ENCODING)
 
-HIGHEST_TEMPERATURE = {
-    'ko':"최고기온(°C)",
-    'en': 'highest_temperature'
-}
+    df = pd.concat([
+        df_2013,
+        df_2014,
+        df_2015,
+        df_2016,
+        df_2017,
+        df_2018,
+        df_2019,
+        df_2020,
+        df_2021,
+        df_2022,
+    ], axis=0)
 
-DAILY_PERCIPITATION = {
-    'ko': '일강수량(mm)',
-    'en': 'daily_percipitation'
-}
+    df = df[[
+        DAY,
+        AVERAGE_TEMPERATURE,
+        LOWEST_TEMPERATURE,
+        HIGHEST_TEMPERATURE,
+        HIGHEST_WIND_SPEED,
+        AVERAGE_WIND_SPEED,
+        AVERAGE_RELATIVE_HUMIDITY
+    ]]
 
-HIGHEST_WIND_SPEED = {
-    'ko': '최대 풍속(m/s)',
-    'en': 'highest_wind_speed'
-}
+    df = df.loc[df[DAY].isin(
+        [
+            '2013-09-27',
+            '2014-09-27',
+            '2015-09-27',
+            '2016-09-27',
+            '2017-09-27',
+            '2018-09-27',
+            '2019-09-27',
+            '2020-09-27',
+            '2021-09-27',
+            '2022-09-27',
+        ]
+    )]
 
-AVERAGE_WIND_SPEED = {
-    'ko': '평균 풍속(m/s)',
-    'en': 'average_with_speed'
-}
+    return df
 
-AVERAGE_RELATIVE_HUMIDITY = {
-    'ko': '평균 상대습도(%)',
-    'en': 'average_relative_humidity'
-}
+def predict(dataframe, target):
+    '''
+    @see https://machinelearningmastery.com/time-series-forecasting-methods-in-python-cheat-sheet/ 
+    '''
+    model = AutoReg(np.asarray(dataframe[target]), lags=1)
+    model_fit = model.fit()
 
-ENCODING = 'cp949' # utf8 makes encoding issue. @see https://zephyrus1111.tistory.com/39
-df_2013 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2013_2013_2015.csv', encoding=ENCODING)
-df_2014 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2014_2014_2015.csv', encoding=ENCODING)
-df_2015 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2015_2015_2016.csv', encoding=ENCODING)
-df_2016 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2016_2016_2017.csv', encoding=ENCODING)
-df_2017 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2017_2017_2018.csv', encoding=ENCODING)
-df_2018 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2018_2018_2019.csv', encoding=ENCODING)
-df_2019 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2019_2019_2020.csv', encoding=ENCODING)
-df_2020 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2020_2020_2021.csv', encoding=ENCODING)
-df_2021 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2021_2021_2022.csv', encoding=ENCODING)
-df_2022 = pd.read_csv('./dataset/SURFACE_ASOS_108_DAY_2022_2022_2023.csv', encoding=ENCODING)
+    predicted = model_fit.predict(start=len(dataframe), end=len(dataframe))
+    return predicted
 
-df = pd.concat([
-    df_2013,
-    df_2014,
-    df_2015,
-    df_2016,
-    df_2017,
-    df_2018,
-    df_2019,
-    df_2020,
-    df_2021,
-    df_2022,
-], axis=0)
+def app():
+    df = get_dataframe()
+    print('---- observed metrics ----')
+    print(df)
 
-df = df.loc[:, [
-    DAY['ko'],
-    AVERAGE_TEMPERATURE['ko'],
-    LOWEST_TEMPERATURE['ko'],
-    HIGHEST_TEMPERATURE['ko'],
-    DAILY_PERCIPITATION['ko'],
-    HIGHEST_WIND_SPEED['ko'],
-    AVERAGE_WIND_SPEED['ko'],
-    AVERAGE_RELATIVE_HUMIDITY['ko']
-]]
+    predicted = pd.DataFrame({
+        DAY: '2023-09-27',
+        AVERAGE_TEMPERATURE: predict(df, AVERAGE_TEMPERATURE),
+        LOWEST_TEMPERATURE: predict(df, LOWEST_TEMPERATURE),
+        HIGHEST_TEMPERATURE: predict(df, HIGHEST_TEMPERATURE),
+        HIGHEST_WIND_SPEED: predict(df, HIGHEST_WIND_SPEED),
+        AVERAGE_WIND_SPEED: predict(df, AVERAGE_WIND_SPEED),
+        AVERAGE_RELATIVE_HUMIDITY: predict(df, AVERAGE_RELATIVE_HUMIDITY),
+    })
 
-df.columns = [
-    DAY['en'],
-    AVERAGE_TEMPERATURE['en'],
-    LOWEST_TEMPERATURE['en'],
-    HIGHEST_TEMPERATURE['en'],
-    DAILY_PERCIPITATION['en'],
-    HIGHEST_WIND_SPEED['en'],
-    AVERAGE_WIND_SPEED['en'],
-    AVERAGE_RELATIVE_HUMIDITY['en']
-]
+    print('---- predicted ----')
+    print(predicted)
 
-print(df)
-
-'''
-TODO
-- handle NaN
-- predict use AR or MA model
-'''
-
-
-# axs = plt.gca()
-
-# selected_df_2013.plot(kind='line', x=DAY['en'], y=AVERAGE_TEMPERATURE['en'], ax=axs)
-
-# # plt.show()
+if __name__ == '__main__':
+    app()
